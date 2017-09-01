0d3a8961186b14b37a45400a03987a695c9af40d
carlosnat <nateracarlos@gmail.com>
2017-09-01 18:11:27 -0500

    codigo asincronico

--- js/app.js
+++ js/app.js
@@ -5,11 +5,28 @@
         .module('myApp', [
             
         ])
-        .controller('myCtrl', function($scope){
+        .controller('myCtrl', function($scope, $timeout){
             $scope.msg = 'hola mundo';
 
-            
+            primer_paso();
+            segundo_paso();
+            tercer_paso();
 
+
+            function primer_paso(){
+                console.log('primer paso');
+            }
+
+            function segundo_paso(){
+                $timeout(function(){
+                    console.log('segundo paso');                
+                }, 2500);
+
+            }
+
+            function tercer_paso(){
+                console.log('tercer paso');
+            }
 
         });
 
