cec8ccd20f6818276f0c2635b2d8a5b126bfe401
carlosnat <nateracarlos@gmail.com>
2017-09-01 20:04:25 -0500

    test de race y all

--- js/app.js
+++ js/app.js
@@ -2,35 +2,52 @@
     'use strict';
 
     angular
-        .module('myApp', [
-            
-        ])
+        .module('myApp', [])
         .controller('myCtrl', function($scope, $timeout){
             $scope.msg = 'hola mundo';
 
-            primer_paso();
+            primer_paso()
+            .then(function(res){
+                console.log('todas resueltas', res);
+            })
+            .catch(function(err){
+                console.log(err);
+            })
 
-            segundo_paso().then(function(res){
-                tercer_paso(res);
-            });
             
             function primer_paso(){
-                console.log('primer paso');
+                return Promise.race([segundo_paso(), tercer_paso()]);
             }
 
             function segundo_paso(){
                 var promesa = new Promise(function(resolve, reject){
+
+                    var numero = Math.floor((Math.random() * 2000) + 1000);
+
                     $timeout(function(){
-                        console.log('segundo paso');  
-                        resolve('ejecutado con exito');              
-                    }, 2500);
+                        
+                        console.log('segundo paso', numero);
+                        if( numero > 10)
+                            resolve(numero); 
+                        else
+                            reject('no vale el valor');             
+                    }, numero);
                 });
                 return promesa;
             }
 
             function tercer_paso(res){
-                console.log('tercer paso ' + res);
+                var promesa = new Promise(function(resolve, reject){
+                    var numero = Math.floor((Math.random() * 2000) + 1000);
+                    $timeout(function(){
+                        console.log('tercer paso');  
+                        resolve(5);              
+                    }, numero);
+                });
+                return promesa;
             }
+
+           
 
         });
 
